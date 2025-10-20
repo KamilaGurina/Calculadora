7815bc0c1366f3870156bad652a86680bca18c5b
Kamila Gurina <kamiflu1@gmail.com>
2025-10-20 20:09:29 +0200

    Rama suma: cambio en línea de Suma

--- scr/Suma.java
+++ scr/Suma.java
@@ -1,5 +1,7 @@
 public class Suma {
     public static double calcular(double a, double b) {
+        // Cambio en rama suma
+        System.out.println("Calculando Suma (rama suma)");
         return a + b;
     }
 }
