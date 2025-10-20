2b8b3493755faec122fb9a3d7d3b75911286a5d4
Kamila Gurina <kamiflu1@gmail.com>
2025-10-20 20:13:14 +0200

    Rama resta: cambio en la misma línea de Suma

--- scr/Resta.java
+++ scr/Resta.java
@@ -1,5 +1,7 @@
 public class Resta {
     public static double calcular(double a, double b) {
+        // Calculadora.java
+    System.out.println("Suma: " + Suma.calcular(a, b) + " (rama resta)");
         return a - b;
     }
 }
