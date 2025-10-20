4a416daf866ae3b78d7a46abc02bb61630d6fdea
Kamila Gurina <kamiflu1@gmail.com>
2025-10-20 20:20:17 +0200

    Conflicto en Suma resuelto

--- scr/Suma.java
+++ scr/Suma.java
@@ -1,7 +1,5 @@
 public class Suma {
     public static double calcular(double a, double b) {
-        // Cambio en rama suma
-        System.out.println("Cambio rama suma");
         return a + b;
     }
 }
