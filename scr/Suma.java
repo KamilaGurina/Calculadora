64d86870a72e8f99705dbb1e1b7ce64e1668376f
Kamila Gurina <kamiflu1@gmail.com>
2025-10-21 19:22:18 +0200

    Programador A: agrega println en Suma.java para conflicto

--- scr/Suma.java
+++ scr/Suma.java
@@ -1,5 +1,6 @@
 public class Suma {
     public static double calcular(double a, double b) {
+        System.out.println("Suma realizada por Programador A"); //Editado
         return a + b;
     }
 }
