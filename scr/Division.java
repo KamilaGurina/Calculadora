e399d5747da72914345928c9d6aa3459c65bda15
Kamila Gurina <kamiflu1@gmail.com>
2025-10-21 20:06:46 +0200

    HotFix: prevenir división entre 0 en Division.java

--- scr/Division.java
+++ scr/Division.java
@@ -2,7 +2,7 @@ public class Division {
     public static double calcular(double a, double b) {
         if (b == 0) {
             System.out.println("Error: No se puede dividir entre 0");
-            return 0; // o cualquier valor que quieras para manejar el error
+            return 0; // O lanzar una excepción si quieres
         }
         return a / b;
     }
