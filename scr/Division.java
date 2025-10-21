052e4ec9e8ac20e3eea7f784a6e2c974bc5c8ee3
Kamila Gurina <kamiflu1@gmail.com>
2025-10-21 18:48:51 +0200

    Programador A: implementa clase Suma

--- scr/Division.java
+++ scr/Division.java
@@ -1,8 +1,8 @@
 public class Division {
     public static double calcular(double a, double b) {
         if (b == 0) {
-            System.out.println("Error: División entre 0");
-            return 0;
+            System.out.println("Error: No se puede dividir entre 0");
+            return 0; // o cualquier valor que quieras para manejar el error
         }
         return a / b;
     }
--- scr/Resta.java
+++ scr/Resta.java
@@ -1,7 +1,5 @@
 public class Resta {
     public static double calcular(double a, double b) {
-        // Calculadora.java
-    System.out.println("Suma: " + Suma.calcular(a, b) + " (rama resta)");
         return a - b;
     }
 }
